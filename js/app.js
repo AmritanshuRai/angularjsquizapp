d558e33b93f2927f181c78eb38222e2aa2e297f4
AmritanshuRai <amritanshu.rai@kuliza.com>
2018-02-15 21:14:31 +0530

    made front page with bootstrap

---
+++ js/app.js
@@ -0,0 +1,8 @@
+(function () {
+    angular
+        .module("turtleFacts", []);
+})();
+
+
+
+//2nd empty [] in module means we are not fetching turtleFacts module but we are creating it.. it we remove [] it means we are fetching the module
